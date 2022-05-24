cebcee73e32f8c6a2a56991a610ea7e45ef89ce7
sam <schristy@medford.k12.ma.us>
2022-05-24 13:45:22 -0400

    Create NinaGradCap.ino

---
+++ NinaGradCap/NinaGradCap.ino
@@ -0,0 +1,68 @@
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+
+#define BLK     0x0000
+#define BLE     0x001F
+#define RED     0xF800
+#define GRN     0x07E0
+#define CYN     0x07FF
+#define MGA     0xF81F
+#define YEW     0xFFE0
+#define WHE     0xFFFF
+
+static const uint16_t PROGMEM
+
+RGB_bmp[64] = {
+  RED, RED, RED, RED, RED, RED, RED, RED,
+  RED, RED, RED, RED, WHE, RED, RED, RED,
+  RED, RED, RED, WHE, WHE, WHE, RED, RED,
+  BLK, RED, WHE, WHE, WHE, RED, RED, BLK,
+  BLK, RED, WHE, RED, WHE, RED, RED, BLK,
+  BLK, BLK, RED, RED, WHE, RED, BLK, BLK,
+  BLK, BLK, BLK, RED, WHE, RED, BLK, BLK,
+  BLK, BLK, BLK, BLK, RED, BLK, BLK, BLK,
+};
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(11, 12, PIN,
+                            NEO_MATRIX_BOTTOM     + NEO_MATRIX_LEFT +
+                            NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+                            NEO_GRB            + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255)
+};
+
+void setup() {
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(250);
+  matrix.setTextColor(colors[0]);
+
+  //for (int x = 0; x < 9; x++) {
+  matrix.drawRGBBitmap(0, 0, (const uint16_t *) RGB_bmp, 8, 8);
+  matrix.show();
+  //   delay(300);
+  //   matrix.clear();
+  // }
+  while (1);
+}
+int x    = matrix.width();
+int pass = 0;
+
+void loop() {
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 0);
+  matrix.print(F("HARVARD"));
+  if (--x < -36) {
+    x = matrix.width();
+    if (++pass >= 3) pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+  matrix.show();
+  delay(100);
+
+}
